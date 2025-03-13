6d6151c89d2d6392ad19042c57d33ec8ec245405
Pham Tien Dat <tiendat12c6ncthanh@gmail.com>
2025-03-13 16:28:11 +0700

    giuaki

---
+++ giuaki/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Hàm hiển thị thông báo chào mừng
+function showWelcomeMessage() {
+    alert("Chào mừng bạn đến với trang nhóm của tôi !");
+}
+
+// Gọi hàm khi trang được tải
+window.onload = showWelcomeMessage;
